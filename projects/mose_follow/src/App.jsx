eec8eeb6206347c257de086cc0c9d6657d8546bc
Fernando Vadillo <gamva04333@gmail.com>
2025-05-13 18:02:42 -0600

    full proyecto

--- projects/mose_follow/src/App.jsx
+++ projects/mose_follow/src/App.jsx
@@ -1,13 +1,60 @@
+import { useEffect, useState } from "react";
 
+const FollowMouse = ({ position, enable, setEnable }) => {
 
-function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <h3>
-      Proyecto 3
-    </h3>
-  )
+    <>
+      <div
+        style={{
+          position: "absolute",
+          background: "#09f",
+          borderRadius: "50%",
+          opacity: 0.8,
+          pointerEvents: "none",
+          left: -20,
+          top: -20,
+          width: 40,
+          height: 40,
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
+      ></div>
+      <button onClick={() => setEnable(!enable)}>
+        {enable ? "Desactivar" : "Activar"} Seguir Puntero
+      </button>
+    </>
+  );
+};
+
+function App() {
+const [enable, setEnable] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      const { clientX, clientY } = event;
+      setPosition({ x: clientX, y: clientY });
+    };
+
+    if (enable) {
+      window.addEventListener("mousemove", handleMouseMove);
+    }
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, [enable]);
+
+  const [mounted, setMounted] = useState(true);
+  return (
+    <main>
+    {mounted && <FollowMouse position={position} enable={enable} setEnable={setEnable} />}
+    <button onClick={() => setMounted(!mounted)}>
+      Toggle FollowMouse
+    </button>
+    </main>
+  );
 }
 
-export default App
+export default App;
